c082d7a63a0bfeb37e50b54c8b3aa2db828149b8
Vurmaz <sadikvurmaz@gmail.com>
2024-07-24 18:37:54 +0300

    add final

--- app/component/Footer.js
+++ app/component/Footer.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Footer() {
   return (
     <div className='flex justify-center items-center bg-apricot h-20 bottom-0 left-0 w-full text-white text-center'>
-        <Image src='/file.png' height={80} width={80}></Image>
+        <Image src='/file.png' height={80} width={80} alt='apricot'></Image>
         <a href="https://www.freepik.com/free-photo/plant-fertilizer-eco-friendly-packaging-bag_17229471
             .htm#query=seed%20packets&position=20&from_view=keyword&track=ais_user&uuid=6d1898eb-30c1-4526-a9b7-67f0d96dce75">
             Image by rawpixel.com
--- app/component/Header.js
+++ app/component/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <div className='bg-apricot h-20 fixed top-0 left-0 w-full text-white grid grid-cols-2 header'>
     <div className='flex items-center ml-8'>
-      <Image src='/file.png' height={80} width={80} className='hidden sm:block'></Image>
+      <Image src='/file.png' height={80} width={80} className='hidden sm:block' alt='apricot'></Image>
         <h1 className='text-lg'>APRI-SEED</h1>
     </div>
     <div className='text-white flex justify-end items-center mr-8 gap-8 cursor-pointer'>
